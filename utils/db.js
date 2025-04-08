89784a63c95a83fbabe0f083132719c1fa3f775c
Domakingo <domakingtheking@gmail.com>
2025-04-08 16:09:37 +0200

    Refactor database connection to export connection pool directly

--- utils/db.js
+++ utils/db.js
@@ -1,23 +1,6 @@
 const mysql = require('mysql2/promise');
-const { DB } = require('./config');
+const { DB } = require('../config/config');
 
-class DatabaseManager {
-    constructor() {
-        this.pool = mysql.createPool(DB);
-    }
+const pool = mysql.createPool(DB);
 
-    async getRuolo(idUser) {
-        const connection = await this.pool.getConnection();
-        try {
-            const [rows] = await connection.execute(
-                "SELECT ruolo FROM Utente WHERE idUtente = ?",
-                [idUser]
-            );
-            return rows[0]?.ruolo || null;
-        } finally {
-            connection.release();
-        }
-    }
-}
-
-module.exports = DatabaseManager;
+module.exports = pool;
