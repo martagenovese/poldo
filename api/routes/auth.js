2b3f6afb4b9c8d2af4f6d2e624aa2c0475ad7831
Giacomo-Marconi <19894@studenti.marconiverona.edu.it>
2025-05-14 16:06:47 +0200

    Corretto il reindirizzamento nel callback di Google OAuth per includere il protocollo HTTP

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -60,7 +60,7 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
     
     //TODO: cambiare
     //res.status(200).json({token: token});
-    res.redirect(`localhost:5173/callback#token=${token}`);
+    res.redirect(`http://localhost:5173/callback#token=${token}`);
 });
 
 router.post('/login', async (req, res) => {
